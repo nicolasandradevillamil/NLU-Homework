8827a494ac188a1dda5b27f096fdd3f5d30f76f4
Nicolas Andrade <nandradev@uniandes.edu.co>
2023-09-04 02:01:09 +0000

    updatepreprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -4,12 +4,11 @@ def create_full_recipes(dataset):
     """
     Creates a new column that includes a full recipe with name, ingredients and steps.
     """
-    fullrecipe= [
-        entry1 + " " + entry2 + " " + entry3
+    fullrecipe= [ " BEGINRECIPE " +
+        entry1 + " \nIngredients:" + entry2 + " \nSteps: " + entry3 + " ENDRECIPE "
         for entry1, entry2, entry3 in zip(dataset[:]['name'],dataset[:]['ingredients'],dataset[:]['steps'])
     ]
     dataset = dataset.add_column(name="recipe",column=fullrecipe)
-    dataset = pd.DataFrame(dataset)
     return dataset
     
 
