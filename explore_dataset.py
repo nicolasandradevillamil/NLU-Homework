beac6d58afb2b6d2c494fe42be04f28a9ae54ef8
Nicolas Andrade <nandradev@uniandes.edu.co>
2023-09-04 01:57:01 +0000

    explore_dataset_update

--- explore_dataset.py
+++ explore_dataset.py
@@ -1,19 +1,16 @@
 #Use this file to answer the "Explore the dataset" section questions. 
-
-from datasets import load_dataset_builder, load_dataset
-from transformers import AutoTokenizer
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
-from transformers import Trainer, TrainingArguments
 import pandas as pd
+from datasets import load_dataset
 from preprocessing import create_full_recipes
 
 #Prepare and load dataset
 train_dataset = load_dataset("m3hrdadfi/recipe_nlg_lite",split="train")
 test_dataset = load_dataset("m3hrdadfi/recipe_nlg_lite",split="test")
 
-#Create a new "Full recipe" column
-train_dataset = create_full_recipes(train_dataset) #See preprocessing.py
-test_dataset = create_full_recipes(test_dataset)
+#Create a new "Full recipe" column, converting to pandas dataframe
+train_dataset = pd.DataFrame(create_full_recipes(train_dataset)) #See preprocessing.py
+test_dataset = pd.DataFrame(create_full_recipes(test_dataset))
+
 #TODO 
 #1) Find out how many recipes are in the train and test sets
 #2) Find out what columns does the dataset have and what each one corresponds to
